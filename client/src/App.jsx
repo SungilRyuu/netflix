db704318e0070c1c6c6db11ce312b64e22466221
sungilRyuu <x7nxc3@gmail.com>
2022-06-22 21:26:49 +0900

    0622

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,18 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Home type="movie" />} />
+        <Route path="/series" element={<Home type="series" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,11 +3,11 @@ import List from "../../component/list/List";
 import Navbar from "../../component/navbar/Navbar";
 import "./home.scss";
 
-const Home = () => {
+const Home = ({ type }) => {
   return (
     <div className="home">
       <Navbar />
-      <Featured />
+      <Featured type={type} />
       <List />
       <List />
       <List />
